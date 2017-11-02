b7b43326a7a188c8a1733ff09d59e191edfd5eef
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-11-02 19:07:31 +0100

    First tests

--- src/main/kotlin/BowlingScoreCalculator.kt
+++ src/main/kotlin/BowlingScoreCalculator.kt
@@ -1,4 +1,10 @@
 
-fun calculate(rolls: String) {
-
+fun calculate(rolls: String): Int {
+  var score = 0
+  for (roll in rolls) {
+    score += calculateScore(roll)
+  }
+  return score
 }
+
+fun calculateScore(roll: Char) = String(charArrayOf(roll)).toInt()
--- src/test/kotlin/BowlingScoreCalculatorShould.kt
+++ src/test/kotlin/BowlingScoreCalculatorShould.kt
@@ -1,9 +1,14 @@
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class BowlingScoreCalculatorShould {
 
   @Test
   fun calculate_score_for_simple_roll() {
-
+    assertThat(calculate("0"), `is`(0))
+    assertThat(calculate("1"), `is`(1))
+    assertThat(calculate("2"), `is`(2))
+    assertThat(calculate("23"), `is`(5))
   }
 }
